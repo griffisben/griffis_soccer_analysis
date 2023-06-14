265fb1d55ec85d554d9f08457c75a3cef00e1016
Ben Griffis <griffis.ben16@gmail.com>
2023-06-14 11:56:23 -0400

    Update

---
+++ Griffis-Soccer-Analysis/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setup(
+    name='Griffis_Soccer_Analysis',
+    version='0.0.1',
+
+    url='https://github.com/griffisben/Griffis-Soccer-Analysis',
+    author='Ben Griffis',
+    author_email='1.fcgriffisconsulting@gmail.com',
+    
+    packages=['Griffis_Soccer_Analysis']
+    install_requires=['pandas','matplotlib','seaborn','numpy','sklearn'],
+)
