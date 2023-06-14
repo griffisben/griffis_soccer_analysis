d2c7c071a3ac6caba7b50a080cbf2a5c70bde814
Ben Griffis <griffis.ben16@gmail.com>
2023-06-14 13:26:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setup(
     name='Griffis_Soccer_Analysis',
-    version='0.0.1',
+    version='1.0.0',
 
     url='https://github.com/griffisben/Griffis-Soccer-Analysis',
     author='Ben Griffis',
     author_email='1.fcgriffisconsulting@gmail.com',
     
-    packages=['Griffis_Soccer_Analysis']
+    packages=['Griffis_Soccer_Analysis'],
     install_requires=['pandas','matplotlib','seaborn','numpy','sklearn'],
 )
