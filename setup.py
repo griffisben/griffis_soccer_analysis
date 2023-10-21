2baf67f71e3419f9da09846f2399509522c9fc8c
Ben Griffis <griffis.ben16@gmail.com>
2023-10-21 15:55:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='griffis_soccer_analysis',
-    version='1.0.1',
+    version='1.0.2',
 
     url='https://github.com/griffisben/griffis_soccer_analysis',
     author='Ben Griffis',
